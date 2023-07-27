b3419b9e44729b94fec834b98a0fb90f6591cd7c
hashmatullah nazari <hn0153@gmail.com>
2023-07-26 19:53:44 -0500

    added redirect page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom'; // Update import statement
 import myImage from './images/lake.png';
 import './App.css';
+import Ai from './ai.js';
 
 function App() {
   return (
-    <div className="container">
-      <div className="column">
-      <img src={myImage} alt="My Image" />
-        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis quisquam illum laudantium dolorum possimus
-           repellendus nam eos, explicabo, reiciendis soluta eius iusto velit. Tempora dolorum fugit temporibus quisquam minima corrupti?</p>
+    <Router>
+      <div className="container">
+        <div className="column">
+          <img src={myImage} alt="My Image" />
+          <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis quisquam illum laudantium dolorum possimus
+            repellendus nam eos, explicabo, reiciendis soluta eius iusto velit. Tempora dolorum fugit temporibus quisquam minima corrupti?</p>
+          <Link className="custom-link" to="/ai">Link to Next Page</Link>
+        </div>
+        <div className="column">
+          <img src={myImage} alt="My Image" />
+          <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis quisquam illum laudantium dolorum possimus
+            repellendus nam eos, explicabo, reiciendis soluta eius iusto velit. Tempora dolorum fugit temporibus quisquam minima corrupti?</p>
+            <Link className="custom-link" to="/ai">Link to Next Page</Link>
+        </div>
+        <div className="column">
+          <img src={myImage} alt="My Image" />
+          <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis quisquam illum laudantium dolorum possimus
+            repellendus nam eos, explicabo, reiciendis soluta eius iusto velit. Tempora dolorum fugit temporibus quisquam minima corrupti?</p>
+            <Link className="custom-link" to="/ai">Link to Next Page</Link>
+        </div>
+
+        {/* Use Routes instead of Switch */}
+        <Routes>
+          <Route path="/ai" element={<Ai />} />
+        </Routes>
       </div>
-      <div className="column">
-      <img src={myImage} alt="My Image" />
-      <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis quisquam illum laudantium dolorum possimus
-           repellendus nam eos, explicabo, reiciendis soluta eius iusto velit. Tempora dolorum fugit temporibus quisquam minima corrupti?</p>
-      </div>
-      <div className="column">
-      <img src={myImage} alt="My Image" />
-      <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis quisquam illum laudantium dolorum possimus
-           repellendus nam eos, explicabo, reiciendis soluta eius iusto velit. Tempora dolorum fugit temporibus quisquam minima corrupti?</p>
-      </div>
-    </div>
+    </Router>
   );
 }
 
---
+++ src/ai.js
@@ -0,0 +1,19 @@
+// ai.js
+import React from 'react';
+
+const Ai = () => {
+  return (
+    <div className="container">
+      <div className="input-field">
+        <label htmlFor="input1">Input 1:</label>
+        <input type="text" id="input1" name="input1" />
+      </div>
+      <div className="input-field">
+        <label htmlFor="input2">Input 2:</label>
+        <input type="text" id="input2" name="input2" />
+      </div>
+    </div>
+  );
+};
+
+export default Ai;
